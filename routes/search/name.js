1080438d98657d80cd272fb11b5596c786f32811
jh0shin <59114042+jh0shin@users.noreply.github.com>
2020-03-24 20:14:48 +0900

    Fix search query for paging 0324

--- routes/search/name.js
+++ routes/search/name.js
@@ -6,18 +6,32 @@ var router = express.Router();
 var mysqlDB = require('../../db_connector');
 
 router.post('/', function (req, res, next) {
-    var hk_name = req.body.hakwonName;
+    var name = req.body.name;
+    var limit = req.body.limit;
+    var offset = req.body.offset;
+    
+    // limit : the number of items in one page
+    // offset : starting index of search result
+    //     ex) 10 items per page and want to get second page,
+    //         (index starting from 0)
+    //         limit is 10 and offset is (limit * (index)) = 10 * 1 = 10
+
+    // If there is 15 result and limit is 10, offset is 10,
+    // 11st ~ 15th items returned.
+
+    // If there is 15 result and limit is 10, offset is 20,
+    // return empty set.
 
     // log search data
     mysqlDB.query(
         'insert into searchlog (time, keyword) values (?, ?);',
-        [Date().toString(), hk_name], function(err, result){}
+        [Date().toString(), name], function(err, result){}
     );
 
     // return search result
     mysqlDB.query(
-        'select * from hk_gs where binary hname like ? limit 200',
-        ["%" + hk_name + "%"], function (err, result){
+        'select * from hk_gs where binary hname like ? limit ? offset ?;',
+        ["%" + name + "%", itemPerPage, limit, offset], function (err, result){
             res.send(result);
     });
 });
