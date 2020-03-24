b10553fadce0c3c415ace509b12e71882d52127f
jh0shin <59114042+jh0shin@users.noreply.github.com>
2020-03-24 17:03:36 +0900

    fix search/name query + log search data 0324

--- routes/search/name.js
+++ routes/search/name.js
@@ -8,9 +8,15 @@ var mysqlDB = require('../../db_connector');
 router.post('/', function (req, res, next) {
     var hk_name = req.body.hakwonName;
 
+    // log search data
+    mysqlDB.query(
+        'insert into searchlog (time, keyword) values (?, ?);',
+        [Date().toString(), hk_name], function(err, result){}
+    );
+
     // return search result
     mysqlDB.query(
-        'select * from hk_gs where binary hname like ?',
+        'select * from hk_gs where binary hname like ? limit 200',
         ["%" + hk_name + "%"], function (err, result){
             res.send(result);
     });
