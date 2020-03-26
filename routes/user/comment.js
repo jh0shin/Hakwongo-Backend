ebe2b15123a148625b6cc7b24f6cfd9ae2c367e0
jh0shin <59114042+jh0shin@users.noreply.github.com>
2020-03-26 17:50:43 +0900

    Fix user bookmark & comment query 0326

--- routes/user/comment.js
+++ routes/user/comment.js
@@ -7,11 +7,15 @@ var mysqlDB = require('../../db_connector');
 
 router.post('/', function (req, res, next) {
     var user = req.body.user;
+    var limit = req.body.limit;
+    var offset = req.body.offset;
+
+    // example in search/name.js
 
     // return search result
     mysqlDB.query(
-        'select * from academy_comment where binary user = ?',
-        [user], function (err, result){
+        'select * from academy_comment where binary user = ? limit ?, ?',
+        [user, Number(offset), Number(limit)], function (err, result){
             res.send(result);
     });
 });
