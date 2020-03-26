a7c6c77014746dc3874777dc9a65b1e3f6f7c86e
jh0shin <59114042+jh0shin@users.noreply.github.com>
2020-03-26 15:37:23 +0900

    Fix getComment query

--- routes/academy/getComment.js
+++ routes/academy/getComment.js
@@ -7,11 +7,15 @@ var mysqlDB = require('../../db_connector');
 
 router.post('/', function (req, res, next) {
     var name = req.body.name;
+    var limit = req.body.limit;
+    var offset = req.body.offset;
+
+    // Example and explain is in search/name.js
 
     // return search result
     mysqlDB.query(
-        'select * from academy_comment where binary name = ?',
-        [name], function (err, result){
+        'select * from academy_comment where binary name = ? order by time desc limit ?, ?',
+        [name, Number(offset), Number(limit)], function (err, result){
             res.send(result);
     });
 });
