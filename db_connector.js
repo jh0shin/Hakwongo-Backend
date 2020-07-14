86acc6481d30a996f5c6ee0392a8eb027b5cefb0
jh0shin <59114042+jh0shin@users.noreply.github.com>
2020-07-14 15:13:35 +0900

    Update db_connector.js

--- db_connector.js
+++ db_connector.js
@@ -8,5 +8,15 @@ var connection = mysql.createConnection({
     database: 'hakwongo',
     charset : 'utf8'
 });
+var connection2 = mysql.createConnection({
+    // host: '49.50.162.157',
+    host: 'localhost',
+    port: 3306,
+    user: 'jh0shin',
+    password: 'hak@won@go1!!',
+    database: 'hwgo',
+    charset : 'utf8'
+});
 
 module.exports = connection;
+module.exports = connection2;
