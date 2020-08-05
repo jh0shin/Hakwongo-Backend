e0c115c52176731e76328aad10bed777a363f800
jh0shin <59114042+jh0shin@users.noreply.github.com>
2020-08-05 12:50:03 +0900

    Update gettest.js

--- routes/api/learningtest/gettest.js
+++ routes/api/learningtest/gettest.js
@@ -7,11 +7,13 @@ var mysqlDB = require('../../../db_connector');
 
 router.post('/', function (req, res, next) {
     var user = req.body.user;
+    var limit = req.body.limit;
+    var offset = req.body.offset;
 
     // return if payment info is existed
     mysqlDB.query(
-        'select * from learningtest where user=?;',
-        [user], function (err, result){
+        'select * from learningtest where user=? order by testtime desc limit ?, ?;',
+        [user, Number(offset), Number(limit)], function (err, result){
             res.send(result);
     });
     
